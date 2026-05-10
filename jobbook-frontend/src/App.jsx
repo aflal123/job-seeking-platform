53ad4bb05b320a0a5028f3271c15b437a8bfad4b
“aflal123” <aflal100ahamed@gmail.com>
2026-05-10 16:35:49 +0530

    feat: add premium landing page at root route

--- jobbook-frontend/src/App.jsx
+++ jobbook-frontend/src/App.jsx
@@ -10,10 +10,13 @@ import Courses from './pages/Courses'
 import Profile from './pages/Profile'
 import Community from './pages/Community'
 
+import Landing from './pages/Landing'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/verify-otp" element={<VerifyOtp />} />
